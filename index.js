f675f984e5464c1bf98b4b4161cd11bbf8cecedc
saidbek <106152417+Saidbek2409@users.noreply.github.com>
2022-10-26 22:32:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+// let int = prompt('любое число')
+// if(int % 2 === 1 ) {
+//     console.log("not even")
+// } else (
+//     console.log( "even")
+// )
+
+
+
+// // 2 //
+
+// let name = prompt('как тебя зовут')
+// if(name[0] === "A" ){
+//     console.log('You can go through')
+// } else {
+//     console.log('go home')
+// }
+
+
+
+// // 3 //
+
+// let age = prompt('сколько тебе лет ')
+// if(age >= '18'){
+//     console.log('You can go through')
+// } else {
+//     console.log('go home')
+// }
+        
+
+let username = prompt("как вас зовут")
+let userRWD = prompt( ' ваш пароль ')
+let usermoney = prompt('сколько обналичить ')
+let remainder = 'на вашей карте '
+let money = +10000
+let name = 'Alex'
+let RWD = 7777
+let a = money
+let b = '-'
+let c = usermoney
+if( username === name ) {
+    console.log("ok")
+}
+
+else if(userRWD === RWD ) {
+    console.log('ok')
+} else{
+    console.log('wrong')
+}
+
+ if(usermoney <= money){
+    console.log('перевод успешно удался')
+}   else{
+    console.log('перевод не удался')
+}
+ 
+
+if(b === '-'){
+    console.log(a-a-c)
+    console.log(a-c)
+    
+}
+else{
+    console.log('Не достаточно средств')
+}
+
+
+
+
+
+
+
+
+
+
+
+let name2 = prompt('Как тебя зовут')
+let age = prompt('Сколько вам лет')
+let money2 = prompt('Сколько у вас денег')
+
+if(name2[0] === "A" ){
+    console.log('You can go through')
+} else {
+    console.log('go home')
+}
+
+if(age >= 20 && age <= 40){
+    console.log('You can go through')
+} else{
+    console.log('go home')
+}
+
+if(money2 >= 100){
+    console.log('проохадите у вас достаточно $')
+} else{
+    console.log('у вас не  достаточно $')
+}
+
+
